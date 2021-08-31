b432a33184ec4e1a288c5d33fcee6e080ea21da5
YagoAlves <yago@heavyconnect.com>
2021-08-30 23:28:18 -0300

    [Yago] Adding movies on admin and swagger documentation

--- movies_api/movies_api/settings.py
+++ movies_api/movies_api/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'movies',
     'django_filters',
+    'drf_yasg',
 ]
 
 REST_FRAMEWORK = {
--- movies_api/movies_api/urls.py
+++ movies_api/movies_api/urls.py
@@ -17,12 +17,26 @@ from django.contrib import admin
 from django.urls import path
 from rest_framework import routers
 from movies.view_sets import MoviesViewSet
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Movies API",
+      default_version='v1',
+      description="Movies database from imdb. Use /movies/ end point to access the data",
+      contact=openapi.Contact(email="yago.alves105@gmail.com"),
+   ),
+   public=True,
+)
 
 router = routers.SimpleRouter()
 router.register(r'movies', MoviesViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('documentation/', schema_view.with_ui('swagger', cache_timeout=0), 
+         name='schema-swagger-ui'),
 ]
 
 urlpatterns += router.urls
